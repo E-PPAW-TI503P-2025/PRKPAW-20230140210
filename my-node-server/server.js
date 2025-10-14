b367d4ce8a8cc40e411982f6ae54665a42895f3a
RafaPramudhita <rafapram360@gmail.com>
2025-10-14 21:14:38 +0700

    Tugas 2

--- my-node-server/server.js
+++ my-node-server/server.js
@@ -1,19 +1,27 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 5000; 
+const PORT = 3001;
 
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
+  next();
+});
+
 app.get('/', (req, res) => {
-  res.send('Server berjalan dengan baik!');
+  res.send('Home Page for API - Server berjalan lancar!');
 });
 
-app.get('/api', (req, res) => {
-  res.json({ message: 'Hello from Node.js Server!' });
+const bookRoutes = require('./routes/books');
+app.use('/api/books', bookRoutes);
+
+app.use((req, res) => {
+  res.status(404).json({ message: '404 Not Found' });
 });
 
-app.listen(port, () => {
-  console.log(`🚀 Server running on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Express server running at http://localhost:${PORT}/`);
 });
